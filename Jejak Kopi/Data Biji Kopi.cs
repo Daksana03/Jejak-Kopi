1c120664c597ef47412bacd9faf9f1e7a0f270de
Daksana03 <dyoprayugo@gmail.com>
2026-06-03 10:10:25 +0700

    perubahan warna

--- Jejak Kopi/Data Biji Kopi.cs
+++ Jejak Kopi/Data Biji Kopi.cs
@@ -10,11 +10,21 @@ namespace Jejak_Kopi
 {
     public partial class Data_Biji_Kopi : Form
     {
+        private readonly string adminName; // add this field
+
         public Data_Biji_Kopi(string adminName)
         {
             InitializeComponent();
+            this.adminName = adminName;     // assign the field
             label2.Text = adminName;
             label4.Text = adminName;
+        }
+
+        private void Daftar_pelanggan_btn_Click(object sender, EventArgs e)
+        {
+            Daftar_Pelanggan daftar = new Daftar_Pelanggan(this.adminName);
+            daftar.Show();
+            this.Hide();
         }
     }
 }
